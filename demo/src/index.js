8ae70732ddbdb3a9a1d402c6b88fc99b37239620
Edgar Muniz Berlinck <eberlinck@PC63353-LINUX.mpes.gov.br>
2019-10-01 14:59:03 -0300

    #21 - Social Icons

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,11 +4,20 @@ import { render } from 'react-dom'
 import SimpleRedButton from '../../src/SimpleRedButton/SimpleRedButton'
 import Button from '../../src/Button/Button'
 import Avatar from '../../src/Avatar/Avatar'
-
+import SocialIcon from '../../src/SocialIcon/SocialIcon'
 class Demo extends Component {
   render () {
     return (
       <div>
+        <SocialIcon name='instagram' size='lg' />
+        <SocialIcon name='instagram' size='md' />
+        <SocialIcon name='instagram' size='sm' />
+        <SocialIcon name='facebook' size='lg' />
+        <SocialIcon name='twitter' size='lg' />
+        <SocialIcon name='pinterest' size='lg' />
+        <SocialIcon name='telegram' size='lg' />
+        <SocialIcon name='whatsapp' size='lg' />
+        <SocialIcon name='other' size='lg' />
         <Avatar src='https://pickaface.net/gallery/avatar/unr_test_161024_0535_9lih90.png' />
         <Button
           text={'best button ever'}
