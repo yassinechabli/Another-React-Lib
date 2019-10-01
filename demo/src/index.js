d63f35577f3d58547c517ca4438c33a48c7a26ce
Edgar Muniz Berlinck <eberlinck@PC63353-LINUX.mpes.gov.br>
2019-10-01 14:35:18 -0300

    #18 Avatar component

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,20 +1,25 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
 import SimpleRedButton from '../../src/SimpleRedButton/SimpleRedButton'
-import Button from '../../src/Button/Button';
+import Button from '../../src/Button/Button'
+import Avatar from '../../src/Avatar/Avatar'
 
 class Demo extends Component {
-
-
-  render() {
-    return <div>
-     <Button 
-     text={'best button ever'} size='medium' styles={{color:'white'}} buttonColor='blue'
-     handleClick = {() => alert('hello world')}
-     />
-    </div>
+  render () {
+    return (
+      <div>
+        <Avatar src='https://pickaface.net/gallery/avatar/unr_test_161024_0535_9lih90.png' />
+        <Button
+          text={'best button ever'}
+          size='medium'
+          styles={{ color: 'white' }}
+          buttonColor='blue'
+          handleClick={() => alert('hello world')}
+        />
+      </div>
+    )
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector('#demo'))
