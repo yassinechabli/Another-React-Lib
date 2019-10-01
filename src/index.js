83f34d40c3cb9d4d876e930b6bed26d235ffbe0f
JuanSimon <jperez@mediastre.am>
2019-10-01 14:06:27 -0500

    feat add ProgressBar component
    add documentation

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import LoadingButton from "./LoadingButton";
 import SimpleRedButton from "./SimpleRedButton/SimpleRedButton";
 import Button from "./Button/Button";
 import Header from "./Header/Header";
+import ProgressBar from "./ProgressBar/ProgressBar"
 
-export { LoadingButton, SimpleRedButton, Button, Header };
+export { LoadingButton, SimpleRedButton, Button, Header, ProgressBar };
