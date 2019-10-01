26eccaf6b6832e222848f4e07fef8736ed83d250
Edgar Muniz Berlinck <eberlinck@PC63353-LINUX.mpes.gov.br>
2019-10-01 16:30:08 -0300

    #26 Automating documentation and adding some example docs

--- src/Avatar/Avatar.js
+++ src/Avatar/Avatar.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import './Avatar.css'
 
+/**
+ * Render a rounded user avatar
+ * E.g.:
+ * ```html
+ * <Avatar src='https://pickaface.net/gallery/avatar/unr_test_161024_0535_9lih90.png' />
+ * ```
+ */
 const Avatar = props => {
   const { src, alt } = props
 
