a31f5e6168cccedcb1bd72a5349992d11ba51adc
Chris Rodgers <devdigression@gmail.com>
2019-10-01 21:43:59 -0400

    add options list

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,67 +1,82 @@
-import React, { Component } from 'react'
-import { render } from 'react-dom'
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
-import SimpleRedButton from '../../src/SimpleRedButton/SimpleRedButton'
+import SimpleRedButton from '../../src/SimpleRedButton/SimpleRedButton';
 
 import Button from '../../src/Button/Button';
-import Form from "../../src/Form/Form";
+import Form from '../../src/Form/Form';
 
 import FlatButton from '../../src/FlatButton/FlatButton';
-import { Radio, RadioGroup} from '../../src/RadioButton/radio'
+import { Radio, RadioGroup } from '../../src/RadioButton/radio';
 
 import Collapse from '../../src/Collapse/Collapse';
 
 import Tabs from '../../src/Tabs/Tabs';
 import Tab from '../../src/Tabs/Tab';
 
+import ExtraOptions from '../../src/ExtraOptions/ExtraOptions';
+
 const Tab1 = () => {
-  return <div>Tab1</div>
-}
+  return <div>Tab1</div>;
+};
 
 const Tab2 = () => {
-  return <div>Tab2</div>
-}
-
-
+  return <div>Tab2</div>;
+};
 
 class Demo extends Component {
-  
-
   render() {
-    const handleChange =  ()=> {
-      console.log("abcd");
+    const handleChange = () => {
+      console.log('abcd');
     };
-    return(
+    return (
       <div>
         <h2>Collapse Component</h2>
 
-        <Collapse collapseHeader={'Click me'} collapseBody={'Sup, dude!'}/>
+        <Collapse collapseHeader={'Click me'} collapseBody={'Sup, dude!'} />
         <br />
         <h2>Button Component</h2>
-        <Button text={'best button ever'} size='medium' styles={{color:'white'}} buttonColor='blue' handleClick = {() => alert('hello world')} />
+        <Button
+          text={'best button ever'}
+          size='medium'
+          styles={{ color: 'white' }}
+          buttonColor='blue'
+          handleClick={() => alert('hello world')}
+        />
         <br />
         <h2>Flat Buttons</h2>
-        <FlatButton text={'best button ever'} size='small' buttonColor='red' handleClick = {() => alert('Flatted world')} />
+        <FlatButton
+          text={'best button ever'}
+          size='small'
+          buttonColor='red'
+          handleClick={() => alert('Flatted world')}
+        />
         <br />
-        <FlatButton text={'best button ever'} size='small' buttonColor='green' handleClick = {() => alert('Flatted world')} />
-        <br/>
-        
+        <FlatButton
+          text={'best button ever'}
+          size='small'
+          buttonColor='green'
+          handleClick={() => alert('Flatted world')}
+        />
+        <br />
+
         <h2>Tab Component</h2>
         <Tabs defaultTab={0}>
-          <Tab component={Tab1} title="Default Tab"/>
-          <Tab component={Tab2} title="Tab 2"/>
+          <Tab component={Tab1} title='Default Tab' />
+          <Tab component={Tab2} title='Tab 2' />
         </Tabs>
 
-<h2>Radio Buttons </h2>
-        <RadioGroup name="xx" selectedValue="Orange" onChange={handleChange}>
-          <Radio value="Orange" /> 
-          <Radio value="Apple" /> 
+        <h2>Radio Buttons </h2>
+        <RadioGroup name='xx' selectedValue='Orange' onChange={handleChange}>
+          <Radio value='Orange' />
+          <Radio value='Apple' />
         </RadioGroup>
+
+        <h2>Extra Options (on hover)</h2>
+        <ExtraOptions />
       </div>
     );
-
   }
 }
 
-render(<Demo />, document.querySelector('#demo'))
-
+render(<Demo />, document.querySelector('#demo'));
--- src/ExtraOptions/ExtraOptions.js
+++ src/ExtraOptions/ExtraOptions.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import './ExtraOptions.css';
 
-const ExtraOptions = props => {
-  return <div></div>;
-};
+class ExtraOptions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hover: false
+    };
+  }
 
-ExtraOptions.defaultProps = {};
+  toggleHover = () => {
+    this.setState({ hover: !this.state.hover });
+  };
+
+  render() {
+    const options = this.props.options.map(option => (
+      <li className='option'>{option}</li>
+    ));
+
+    return (
+      <div
+        className='section'
+        onMouseEnter={this.toggleHover}
+        onMouseLeave={this.toggleHover}
+      >
+        {this.state.hover && <ul className='optionsList'>{options}</ul>}
+      </div>
+    );
+  }
+}
+
+ExtraOptions.defaultProps = {
+  options: ['Delete', 'Save']
+};
 
 export default ExtraOptions;
