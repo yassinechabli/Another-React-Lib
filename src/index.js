b4bb339ec1f6f88bfcc62c6141c001b2c555ec51
Esther <esther@hometalk.com>
2019-10-02 17:20:46 +0300

    Implementing a wrapper that handles different types of alerts

--- src/index.js
+++ src/index.js
@@ -5,35 +5,33 @@ import Button from "./Button/Button";
 import Header from "./Header/Header";
 import Form from "./Form/Form";
 
-import ProgressBar from "./ProgressBar/ProgressBar"
-import FlatButton from './FlatButton/FlatButton';
-import {Radio, RadioGroup} from './RadioButton/radio'
-import Tabs from './Tabs/Tabs';
-import Tab from './Tabs/Tab'
-import Dropdown from './Dropdown/Dropdown';
-import TwitterShare from './TwitterShareButton/twitterShare'
-import Switch from './Switch/Switch'
+import ProgressBar from "./ProgressBar/ProgressBar";
+import FlatButton from "./FlatButton/FlatButton";
+import { Radio, RadioGroup } from "./RadioButton/radio";
+import Tabs from "./Tabs/Tabs";
+import Tab from "./Tabs/Tab";
+import Dropdown from "./Dropdown/Dropdown";
+import TwitterShare from "./TwitterShareButton/twitterShare";
+import Switch from "./Switch/Switch";
+import ExtendedText from "./ExtendedText/ExtendedText";
 
 
 
-export { 
-  LoadingButton, 
-  SimpleRedButton, 
-  GhostButton, 
+export {
+  LoadingButton,
+  SimpleRedButton,
+  GhostButton,
   Button,
-  Header, 
-  FlatButton, 
-  ProgressBar, 
-  Form, 
-  Tabs, 
-  Tab, 
-  Radio, 
-  RadioGroup, 
-  Dropdown, 
+  Header,
+  FlatButton,
+  ProgressBar,
+  Form,
+  Tabs,
+  Tab,
+  Radio,
+  RadioGroup,
+  Dropdown,
   Switch,
   ExtendedText,
   TwitterShare
 };
-
-
-
