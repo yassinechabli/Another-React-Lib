f1ec41db2e53966ccc056147e26868188d237c74
Annaëlle Sagot <asagot@octo.com>
2019-10-03 11:12:36 +0200

    Resolve conflicts

--- src/index.js
+++ src/index.js
@@ -1,48 +1,43 @@
 import LoadingButton from "./LoadingButton";
 import SimpleRedButton from "./SimpleRedButton/SimpleRedButton";
-import Collapse from "./Collapse/Collapse";
-import Profile from "./Profile/Profile";
-import SearchBar from "./SearchBar/Searchbar";
 import GhostButton from "./GhostButton/GhostButton";
-import ExtendedText from "./ExtendedText/ExtendedText";
 import Button from "./Button/Button";
 import Header from "./Header/Header";
 import Form from "./Form/Form";
 
-import ProgressBar from "./ProgressBar/ProgressBar"
-import FlatButton from './FlatButton/FlatButton';
-import {Radio, RadioGroup} from './RadioButton/radio'
-import Tabs from './Tabs/Tabs';
-import Tab from './Tabs/Tab'
-import Dropdown from './Dropdown/Dropdown';
-import TwitterShare from './TwitterShareButton/twitterShare'
-import Switch from './Switch/Switch'
-import Datepicker from './Datepicker/Datepicker'
+import ProgressBar from "./ProgressBar/ProgressBar";
+import FlatButton from "./FlatButton/FlatButton";
+import { Radio, RadioGroup } from "./RadioButton/radio";
+import Tabs from "./Tabs/Tabs";
+import Tab from "./Tabs/Tab";
+import Dropdown from "./Dropdown/Dropdown";
+import TwitterShare from "./TwitterShareButton/twitterShare";
+import Switch from "./Switch/Switch";
+import Collapse from "./Collapse/Collapse";
+import Profile from "./Profile/Profile";
+import SearchBar from "./SearchBar/Searchbar";
+import ExtendedText from "./ExtendedText/ExtendedText";
+import Datepicker from "./Datepicker/Datepicker";
 
-
-
-export { 
-  LoadingButton, 
+export {
+  LoadingButton,
   SimpleRedButton,
-  Collapse,
-  Profile,
-  SearchBar,
-  GhostButton, 
+  GhostButton,
   Button,
-  Header, 
-  FlatButton, 
-  ProgressBar, 
-  Form, 
-  Tabs, 
-  Tab, 
-  Radio, 
-  RadioGroup, 
-  Dropdown, 
+  Header,
+  FlatButton,
+  ProgressBar,
+  Form,
+  Tabs,
+  Tab,
+  Radio,
+  RadioGroup,
+  Dropdown,
   Switch,
   ExtendedText,
   TwitterShare,
+  Collapse,
+  Profile,
+  SearchBar,
   Datepicker
 };
-
-
-
