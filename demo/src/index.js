d122a96c5241191ecb3a5b37655b48a35c122c1a
mkamranhamid <mkamranhamid@gmail.com>
2019-10-05 19:15:00 +0500

    add navbar component

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,20 +1,43 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
 import SimpleRedButton from '../../src/SimpleRedButton/SimpleRedButton'
 import Button from '../../src/Button/Button';
+import { Navbar, NavItems } from "../../src"
 
 class Demo extends Component {
 
 
   render() {
-    return <div>
-     <Button 
-     text={'best button ever'} size='medium' styles={{color:'white'}} buttonColor='blue'
-     handleClick = {() => alert('hello world')}
-     />
-    </div>
+    return (
+      <div>
+        <Navbar theme="">
+          <Navbar.Brand>
+            <span>MYBRAND</span>
+          </Navbar.Brand>
+          <NavItems>
+            <NavItems.Item text="Item1" />
+            <NavItems.Item text="Item2" />
+            <NavItems.Item text="Item3" />
+            <NavItems.Item text="Item4" />
+            <NavItems.Item text="Item5" />
+            <NavItems.Item text="Item6" />
+            <NavItems.Item text="Item7" />
+            <NavItems.Item text="Item8" />
+            <NavItems.Item text="Item9" />
+            <NavItems.Item text="Item10" />
+{/*             <NavItems.Item text="Item11" />
+            <NavItems.Item text="Item12" />
+            <NavItems.Item text="Item13" /> */}
+          </NavItems>
+        </Navbar>
+        <Button
+          text={'best button ever'} size='medium' styles={{ color: 'white' }} buttonColor='blue'
+          handleClick={() => alert('hello world')}
+        />
+      </div>
+    )
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector('#demo'))
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import LoadingButton from './LoadingButton'
 import SimpleRedButton from './SimpleRedButton/SimpleRedButton'
 import Button from './Button/Button'
+import Navbar from './Navbar/Navbar'
+import NavItems from './Navbar/NavItems'
 
-export {LoadingButton, SimpleRedButton, Button}
+export { LoadingButton, SimpleRedButton, Button, Navbar, NavItems }
