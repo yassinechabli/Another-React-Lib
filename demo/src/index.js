79446d8546ffee1de7a6020f7aa163e6d0d12427
Victor Tavarez <victor.j.tavarez@gmail.com>
2019-10-06 15:00:23 -0700

    Profile component pre-set stylesheet created

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,28 +1,31 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 
-import {
-  Button
-} from "../../src/index";
-
+import { Button, Profile } from "../../src/index";
 
 class Demo extends Component {
   render() {
     return (
       <div>
-       <h2>
-       Please feel free to use this file to experiment with components. 
-        However, do not push any changes to this file. 
-         </h2> 
+        <h2>
+          Please feel free to use this file to experiment with components.
+          However, do not push any changes to this file.
+        </h2>
 
-         <h4>Example of a component</h4>
-        <Button buttonColor="blue"/>
-
-
+        <h4>Example of a component</h4>
+        <Button buttonColor="blue" />
+        <Profile
+          className="Twitter"
+          name="Test Name Here"
+          bio="The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog."
+          socials={[
+            { name: "twitter", link: "twiter.com", size: "sm" },
+            { name: "facebook", link: "facebook.com", size: "sm" }
+          ]}
+        />
       </div>
     );
   }
 }
 
-render(<Demo />, document.querySelector('#demo'))
-
+render(<Demo />, document.querySelector("#demo"));
--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,27 +1,38 @@
-import React from 'react';
-import SocialIcon from '../SocialIcon/SocialIcon';
+import React from "react";
+import SocialIcon from "../SocialIcon/SocialIcon";
 //To do: create styling and different pre-set profile style types
+import "./styles.css";
 
-function Profile(props){
-    const {name, image, bio, className, link, socials} = props;
+function Profile(props) {
+  const { name, image, bio, className, link, socials } = props;
 
-    return(
-        <section className={className && className || `${name}-profile`}>
+  return (
+    <section className={(className && className) || `${name}-profile`}>
+      {image && <img src={image.location}></img>}
 
-            <img src={image.location}></img>
+      <h2>{name}</h2>
 
-            <h2>{name}</h2>
+      <p>{bio}</p>
 
-            <p>{bio}</p>
+      {link && <a href={link.source}>{link.text}</a>}
+      {link && <br></br>}
 
-            {link && <a href={link.source}>{link.text}</a>}
-            {link && <br></br>}
-            
-            {socials && <div className = 'social-links'>{socials.map(social => {
-                return <a href={social.source}><SocialIcon name={social.name} size={social.size || 'md'}/></a>
-            })}</div>}
-        </section>
-    )
+      {socials && (
+        <div className="social-links">
+          {socials.map(social => {
+            return (
+              <SocialIcon
+                key={social.name}
+                name={social.name}
+                size={social.size || "md"}
+                link={social.link}
+              />
+            );
+          })}
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default Profile;
